f339b732ac31c089499369e4150c9fe57eb91e2e
Keith Benjamin <kjb528i@yahoo.com>
2022-08-01 18:43:16 -0400

    find clients by attorney id

--- src/main/java/com/timesheettracker/timesheettracker/repositories/MatterRepository.java
+++ src/main/java/com/timesheettracker/timesheettracker/repositories/MatterRepository.java
@@ -1,14 +1,10 @@
 package com.timesheettracker.timesheettracker.repositories;
 
-import com.timesheettracker.timesheettracker.models.Client;
 import com.timesheettracker.timesheettracker.models.Matter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MatterRepository extends JpaRepository<Matter, Long> {
    List<Matter> findAllByClient_id(Long id);
-
 }
